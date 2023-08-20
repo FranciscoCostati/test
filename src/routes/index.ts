2429c978ec314c26884104f3ffadc69345faea1f
FranciscoCostati <francisco_ifcj@outlook.com>
2023-08-20 20:08:08 -0300

    script update

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,15 @@ import { Router, Request, Response } from "express";
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response)=>{ //renderiza a pasta Home (com os arquivos mustache/html)
-    res.render("home")
+router.get("/", (req: Request, res: Response)=>{ 
+    let user = {
+        name: "Laerte",
+        age: "19 anos"
+    };
+
+    res.render("home", { //renderiza a pasta Home (com os arquivos mustache/html)
+      user  //renderiza objeto user para que possa ser usada no arquivo mustache (user.name)
+    })
 });
 
 router.get("/contato", (req: Request, res: Response)=>{ // exibe na tela Formulario de contato
