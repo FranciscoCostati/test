4e10ac6f66f940120b4cd07216a83291bc55c86d
FranciscoCostati <francisco_ifcj@outlook.com>
2023-08-20 18:10:20 -0300

    b7w classes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response} from "express"; // importar express Request e Response
+import path from "path"; // para indicar diretório absoluto
+import varMainRoutes from "./routes/index"; //importa o arquivo de paths para a variavel varMainRoutes
 
-const server = express();
+const server = express(); //salva express em constante server para ser usado
 
-server.get("/voo/:origem-:destino", (req: Request, res: Response) => {
-    let { origem, destino } = req.params
-    res.send(`Search Journet for ${origem.toUpperCase()} to ${destino.toUpperCase()}`);
+server.use(express.static( path.join(__dirname, "../public")));  // determina diretorio absoluto >> __dirmane insica ditetório atual >> ../ indica diretório pai >> pulic indica diretório public
+
+server.use("/", varMainRoutes); //usa a variavel varMainRoutes no caminho / (arquivo de path)
+
+server.use((req: Request, res: Response)=>{  //Cria retorno quando o path não é encontrado
+    res.status(404).send("Pagina não encontrada");
 });
 
-server.listen(80);
+server.listen(80); //insica porta que o servidor ira rodar
 
